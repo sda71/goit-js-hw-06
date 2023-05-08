12643ba623969d7d2090b1ba5b58f5f005cb93e0
Andrii Telichko <sdoctor.teko@gmail.com>
2023-05-09 01:04:04 +0300

    minor-chnge-in-1-task

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,14 @@
 const categoriesList = document.getElementById('categories');
 const categoriesItems = categoriesList.children;
 
-console.log(`Number of categories: ${categoriesItems.length}`);
+console.log('Number of categories: ', categoriesItems.length);
 
 Array.from(categoriesItems).forEach(item => {
   const itemTitle = item.firstElementChild.textContent;
-  console.log(`Category: ${itemTitle}`);
+  console.log('Category: ', itemTitle);
   
   const numOfElements = item.querySelectorAll('li').length;
-  console.log(`Elements: ${numOfElements}`);
+  console.log('Elements: ', numOfElements);
 });
+
+
