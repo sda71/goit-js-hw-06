ab1c2d13287d00053926ba83edbf49ff8648bb6c
Andrii Telichko <sdoctor.teko@gmail.com>
2023-05-09 00:33:12 +0300

    use_children

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,12 @@
-const categoriesList = document.querySelector('#categories');
-
-const categoriesItems = categoriesList.querySelectorAll('.item');
+const categoriesList = document.getElementById('categories');
+const categoriesItems = categoriesList.children;
 
 console.log(`Number of categories: ${categoriesItems.length}`);
 
-categoriesItems.forEach(item =>{
+Array.from(categoriesItems).forEach(item => {
   const itemTitle = item.firstElementChild.textContent;
   console.log(`Category: ${itemTitle}`);
   
   const numOfElements = item.querySelectorAll('li').length;
   console.log(`Elements: ${numOfElements}`);
-   
 });
